6549d1e16992a20f346a4b8d8a6d85acbaa92a90
RamziMammadli <remzimemmedli95@gmail.com>
2023-04-17 23:29:04 +0400

    states commited

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import axios from "axios";
 
 function App() {
 
-  const [baza, setbaza] = useState([]);
+  const [baza, setbaza] = useState([]);   // melumatlari- datani STATE-de saxlayiriq
 
   useEffect(() => {
     axios.get(`https://northwind.vercel.app/api/suppliers`).then((response) => {
-      setbaza(response.data);
+      setbaza(response.data); // response.data ile gelen melumatlari setbaza ile bazaya gonderirik, bazada tuturuq
     });
     }, []);
 
   return (
     <div className="App">
-      {baza.map((item) => (
+      {baza.map((item) => ( // bazani map methodla yazdiririq. item bir obyekti temsil edir deye , her obyektin datasini item.name yaxud item.title kimi yaziriq ve yazdiririq
         <div className="user">
           {item.companyName}
           {item.contactName}
