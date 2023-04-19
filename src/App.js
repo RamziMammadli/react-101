df9151f44b9297cc73259ded30293ff7407087a5
RamziMammadli <remzimemmedli95@gmail.com>
2023-04-19 23:00:10 +0400

    useState useEffect Map method

--- src/App.js
+++ src/App.js
@@ -1,72 +1,38 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+function App() {
+  const [count, setCount] = useState(0);
 
+  const [name, setName] = useState('');
 
-const App = () => {
+  const [ data, setData ] = useState([])
 
-
-  const [count, setCount] = useState()
-  const [name, setName] = useState("Sitare")
-  const [surname, setSurame] = useState("Hummet")
-  const [status, setStatus] = useState(false)
-
-
-  const artir = () => {
+  const Artir = () => {
     setCount(count + 1);
   };
 
-  const azalt = () => {
+  const Azalt = () => {
     setCount(count - 1);
   };
 
-///////////////////// Sitare
-///////////////////// Hummet
+  useEffect(() => {
+    axios.get("https://northwind.vercel.app/api/orders/").then((response) => {
+      setData(response.data)
+    });
+  }, []);
+
 
   return (
-    <div>
-      <h1>Salam, {name} {surname}</h1>
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <button onClick={artir}>Artir</button>
-      <button onClick={azalt}>Azalt</button>    
+    <div className="App">
+      {data.map(item => (<div style={{backgroundColor: "lightblue"}}>
+        <p>{item.shipName}</p>
+        <p>{item.customerId}</p>
+        <p>{item.customerId}</p>
+        <p>{item.customerId}</p>
+      </div>))}
     </div>
   );
-};
+}
 
 export default App;
-
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// function App() {
-
-//   const [baza, setbaza] = useState([]);   // melumatlari- datani STATE-de saxlayiriq
-
-//   useEffect(() => {
-//     axios.get(`https://northwind.vercel.app/api/suppliers`).then((response) => {
-//       setbaza(response.data); // response.data ile gelen melumatlari setbaza ile bazaya gonderirik, bazada tuturuq
-//     });
-//     }, []);
-
-//   return (
-//     <div className="App">
-//       {baza.map((item) => ( // bazani map methodla yazdiririq. item bir obyekti temsil edir deye , her obyektin datasini item.name yaxud item.title kimi yaziriq ve yazdiririq
-//         <div className="user">
-//           {item.companyName}
-//           {item.contactName}
-//         </div>
-//       ))}
-//     </div>
-//   );
-//   }
-
-// export default App;
-
-
-    // axios.get(`https://northwind.vercel.app/api/orders/10271`)
-    // .then(response => {
-    //   response.data.filter(item => item.)
-    // })
-   
